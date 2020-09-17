2c08368ccc35f81990eb2947919d56ae24063b90
Aditya Pathre <pathreaig@gmail.com>
2020-09-18 00:23:38 +0100

    fixed schema

--- dbModel.js
+++ dbModel.js
@@ -1,13 +1,9 @@
 const mongoose=require('mongoose');
 
 const quizbetSchema=mongoose.Schema({
-    url:String,
-    channel:String,
-    song:String,
-    likes:String,
-    messages:String,
-    description:String,
-    shares:String
+    name:String,
+    state:String,
+    country:String
 });
 
 
